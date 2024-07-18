717a1c927ab3cb2a8b72c7c820358f4ef67f758a
Morgan Barber <morgan_e_barber@icloud.com>
2024-07-18 17:14:42 -0600

    autoincrement users in db

--- backend/src/models.py
+++ backend/src/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))

4d6274ce41ad8fb895375fad3f7ff01e6f5819e7
Morgan Barber <morgan_e_barber@icloud.com>
2024-07-19 17:46:54 -0600

    Update schemas.py

--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -27,12 +27,10 @@ class ExpenseSchema(Schema):
     id = fields.Integer(dump_only=True)
     user_id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
-    amount = fields.Float(required=True)
     description = fields.String()
     frequency = fields.Enum(enum=ExpenseFrequency, required=True)
-    category = fields.String(required=True)
     color = fields.String()
     date = fields.Date()
 
     class Meta:
-        fields = ("id", "user_id", "name", "amount", "description", "frequency", "category", "color", "date")
+        fields = ("id", "user_id", "name", "description", "frequency", "color", "date")

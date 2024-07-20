a6b6f056e5a101e60e7b2b4bce1a6f14a51c2dfe
Morgan Barber <morgan_e_barber@icloud.com>
2024-07-19 23:04:05 -0600

    Update models.py

--- backend/src/models.py
+++ backend/src/models.py
@@ -36,7 +36,7 @@ class Income(db.Model):
 
 class Expense(db.Model):
     __tablename__ = 'expense'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     name = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text)

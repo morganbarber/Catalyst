3b67a0a1f151ffd49970cad2815e356924a2ae01
Morgan Barber <morgan_e_barber@icloud.com>
2024-07-21 17:20:54 -0600

    Update enums.py

--- backend/src/enums.py
+++ backend/src/enums.py
@@ -4,6 +4,9 @@ class IncomeFrequency(Enum):
     monthly = 'monthly'
     one_time = 'one_time'
     annually = 'annually'
+    bi_weekly = 'bi_weekly'
+    weekly = 'weekly'
+    daily = 'daily'
 
 class ExpenseFrequency(Enum):
     monthly = 'monthly'

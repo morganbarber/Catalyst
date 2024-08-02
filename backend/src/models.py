e925196dd83cb60e30d16fa60ca8f264a9c1e822
Morgan Barber <morgan_e_barber@icloud.com>
2024-08-02 15:54:27 -0600

    fix formatting

--- backend/src/models.py
+++ backend/src/models.py
@@ -137,3 +137,15 @@ class Goal(db.Model):
 
     def __repr__(self):
         return f'<Goal {self.name} - {self.target_amount} - {self.current_amount}>'
+    
+class Tax(db.Model):
+    __tablename__ = 'tax'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    year = db.Column(db.Integer, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text)
+    optimization_strategy = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Tax {self.year} - {self.amount}>'

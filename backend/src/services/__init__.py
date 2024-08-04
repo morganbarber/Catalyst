02808f310073da70dd111673ce4d229b0029dec4
Morgan Barber <morganbarber928@gmail.com>
2024-08-04 13:13:09 -0600

    a

--- backend/src/services/__init__.py
+++ backend/src/services/__init__.py
@@ -1,3 +1,5 @@
 from .auth import AuthService
 from .income_tracking import IncomeTrackingService
 from .expense_tracking import ExpenseTrackingService
+from .debt_tracking import DebtTrackingService
+from .investment_tracking import InvestmentTrackingService

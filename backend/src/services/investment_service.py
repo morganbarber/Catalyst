7c15c32f947b905e9d70e54f139131712af71e6d
Morgan Barber <morganbarber928@gmail.com>
2024-08-12 09:00:40 -0600

    fixes

--- backend/src/services/investment_service.py
+++ backend/src/services/investment_service.py
@@ -191,10 +191,10 @@ class PortfolioService:
                 if investment.portfolio_id == portfolio.id:
                     
                     recent_data = yf.download(investment.name, start=today - relativedelta(months=6), end=today)
-                    print(recent_data['Close'].values.tolist())
                     historical_data[portfolio.name][investment.name] = recent_data['Close'].values.tolist()
                     for i in historical_data[portfolio.name][investment.name]:
                         historical_data[portfolio.name][investment.name] = i * investment.amount
+                        print(historical_data[portfolio.name][investment.name])
 
         return historical_data
     
